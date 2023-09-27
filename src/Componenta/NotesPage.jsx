cd838b648aca6b6dfcf78e9926c78e301eed93a9
kuranithi <kuralnithi1999@gmail.com>
2023-09-27 09:38:45 +0530

    changes updated

--- src/Componenta/NotesPage.jsx
+++ src/Componenta/NotesPage.jsx
@@ -1,237 +1,171 @@
 import React, { useContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNoteSticky, faPenClip, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faNoteSticky, faPenClip, faTrash } from '@fortawesome/free-solid-svg-icons'; // Updated icons
 import { MyContext } from '../App';
-
+import { v4 as uuidv4 } from 'uuid';
 
 function NotesPage(props) {
-
-    const [inptxt, setInptxt] = useState('')
-
-    const [inptxt2, setInptxt2] = useState('')
-
-
-
-
-
-
-    const { NoteValue, SetNoteValue, TaskValue, SetTaskValue, editId, setEditid, editmode, setEditmode, editedTitle, setEditedtitle, editedContent, setEditedcontent, editNote, setEditnote } = useContext(MyContext);
+    const {
+        NoteValue,
+        SetNoteValue,
+        editId,
+        setEditid,
+        editmode,
+        setEditmode,
+        editedTitle,
+        setEditedtitle,
+        editedContent,
+        setEditedcontent,
+    } = useContext(MyContext);
 
     const DispBoxVal = NoteValue;
 
-    ////////////////////////////////////////
-
-
     const handleChange = (e) => {
-
-
-        setInptxt(e.target.value);
-
-
-
-    }
+        setEditedtitle(e.target.value);
+    };
 
     const handleChange2 = (e) => {
-
-
-        setInptxt2(e.target.value);
-
-
-
-    }
-
-    ///////////////////////////////////////
-
-
-
+        setEditedcontent(e.target.value);
+    };
 
     const handleClick = () => {
-
-
-        SetNoteValue(() => {
-
-
-            let data = {
+        if (editmode) {
+            SetNoteValue((prevNotes) =>
+                prevNotes.map((note) =>
+                    note.id === editId
+                        ? {
+                            ...note,
+                            content1: editedTitle,
+                            content2: editedContent,
+                        }
+                        : note
+                )
+            );
+            setEditmode(false);
+            setEditid('');
+            setEditedtitle('');
+            setEditedcontent('');
+        } else {
+            const data = {
                 id: uuidv4(),
-                content1: inptxt,
-                content2: inptxt2
-
-            }
-
-            console.log(NoteValue)
-            console.log(data);
-            return [...NoteValue, (data)]
-
-        });
-
-        setInptxt('')
-        setInptxt2('')
-
-    }
-
-
-    ////////////////////////////////////
-
-    const handleDelete = (id) => {
-
-
-        SetNoteValue((NoteValue) => {
-            console.log(id);
-
-            const AfterDelete = NoteValue.filter((obj) => obj.id !== id)
-
-            console.log(AfterDelete);
-
-            return AfterDelete;
-
-        })
-    }
-    /////////////////////////////////////////////////////////
-
-
+                content1: editedTitle,
+                content2: editedContent,
+            };
+            SetNoteValue((prevNotes) => [...prevNotes, data]);
+            setEditedtitle('');
+            setEditedcontent('');
+        }
+    };
 
     const handleEdit = (id) => {
-
-        if (editmode == true) {
-            setEditmode(false)
-        }
-
-        else {
-            console.log(id);
+        const selectedNote = DispBoxVal.find((note) => note.id === id);
+        if (selectedNote) {
             setEditmode(true);
             setEditid(id);
-
-            DispBoxVal.map((obj) => {
-
-                if (obj.id == id) {
-
-
-                    setEditedtitle(obj.content1);
-                    setEditedcontent(obj.content2);
-                    console.log(editedTitle);
-                    return obj;
-                }
-                else
-                    return obj;
-
-            })
-
-
-
-
+            setEditedtitle(selectedNote.content1);
+            setEditedcontent(selectedNote.content2);
         }
+    };
 
-        const contentInparr = DispBoxVal.map(obj => {
-
-            if (obj.id == id) {
-
-
-                setEditedtitle(obj.content2);
-
-                return obj;
-            }
-            else
-                return obj;
-
-        }
-
-
-        );
-
-
-
-
-    }
-
-
-
-
-    /////////////////////
-
-
-
-    const handleSave = () => {
-        setEditmode(false);
-
-
-        const editData = {
-            id: editId,
-            Econtent1: editedTitle,
-            Econtent2: editedContent
-        }
-        console.log(editData);
-
-        setEditnote(editData);
-
-        setEditedtitle('');
-        setEditedcontent('');
-    }
-
-    const handleCancel = () => {
-
-
-        setEditmode(false);
-
-    }
-
-
-
-
-
-
-
-
+    const handleDelete = (id) => {
+        SetNoteValue((prevNotes) => prevNotes.filter((note) => note.id !== id));
+    };
 
     return (
-
-        <div className=' addnoteboxmain container-fluid mt-5'>
-            <div className='addnotebox container-fluid '>
-
-                <h1 >Add a Note</h1>
-
-                <input className='addnotetxtbox' type="text" placeholder='Title' value={inptxt} onChange={handleChange} />
-                <input type="text" placeholder='Take a note...' value={inptxt2} onChange={handleChange2} className='mt-2 addnotetxtbox2' />
-
-
-                <div className='timeclickbox'>
-
-
-
-                    <button className='addclick mt-3' onClick={handleClick}>Add</button>
-
-                    <button className='remtime mt-5 mb-2'> Today,10:10 AM</button>
-
-
-
+        <div className="addnoteboxmain container-fluid mt-5">
+            <div className="addnotebox container-fluid">
+                <h1>Add a Note</h1>
+                <input
+                    className="addnotetxtbox"
+                    type="text"
+                    placeholder="Title"
+                    value={editedTitle}
+                    onChange={handleChange}
+                />
+                <textarea
+                    rows={4}
+                    className="addnotetxtbox2 mt-2"
+                    type="text"
+                    placeholder="Take a note..."
+                    value={editedContent}
+                    onChange={handleChange2}
+                />
+                <div className="timeclickbox">
+                    <button className="addclick mt-3" onClick={handleClick}>
+                        {editmode ? 'Save' : 'Add'}
+                    </button>
+                    <button className="remtime mt-5 mb-2">Today, 10:10 AM</button>
                 </div>
-
-
-
             </div>
-
-            <div className='display-container mt-5'>
-                <h1 className='mynotes'> <FontAwesomeIcon icon={faNoteSticky} /> My Notes</h1>
-
+            <div className="display-container mt-5">
+                <h1 className="mynotes">
+                    <FontAwesomeIcon icon={faNoteSticky} /> My Notes
+                </h1>
                 {DispBoxVal.length > 0 ? (
                     <div className="card-container horizontal-card-list mt-1">
                         {DispBoxVal.map((BoxVal) => (
                             <div key={BoxVal.id} className="card">
                                 <div className="card-body">
-
                                     <div className="card-title">
-                                        <h4 className='addnotetxt'> {BoxVal.content1}</h4>
-
-                                        <div className='btnicons '><FontAwesomeIcon icon={faPenClip} onClick={() => { handleEdit(BoxVal.id) }} />
-                                            <button className="deletebtn" onClick={() => { handleDelete(BoxVal.id) }}>
-                                                <FontAwesomeIcon icon={faTrashCan} />   </button>   </div>
-
+                                        {editmode && editId === BoxVal.id ? (
+                                            <>
+                                                <input
+                                                    type="text"
+                                                    className="addnotetxt"
+                                                    value={editedTitle}
+                                                    onChange={handleChange}
+                                                />
+                                                <div className="btnicons">
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-success btn-sm mx-2"
+                                                        onClick={handleClick}
+                                                    >
+                                                        Save
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-sm btn-warning mx-2"
+                                                        onClick={() => {
+                                                            setEditmode(false);
+                                                            setEditid('');
+                                                            setEditedtitle('');
+                                                            setEditedcontent('');
+                                                        }}
+                                                    >
+                                                        Cancel
+                                                    </button>
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <h4 className="addnotetxt">{BoxVal.content1}</h4>
+                                                <div className="btnicons d-flex flex-column">
+                                                    <button
+                                                        className="btn btn-sm btn-primary mx-2"
+                                                        onClick={() => handleEdit(BoxVal.id)}
+                                                    >
+                                                        <FontAwesomeIcon icon={faPenClip} /> 
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-sm btn-danger mx-2"
+                                                        onClick={() => handleDelete(BoxVal.id)}
+                                                    >
+                                                        <FontAwesomeIcon icon={faTrash} />                                                     </button>
+                                                </div>
+                                            </>
+                                        )}
                                     </div>
-
-                                    <div className='card-content'>
-
-                                        <p className='addnotetxt'>  {BoxVal.content2}</p>
-
+                                    <div className="card-content">
+                                        {editmode && editId === BoxVal.id ? (
+                                            <textarea
+                                                rows={4}
+                                                className="addnotetxt"
+                                                value={editedContent}
+                                                onChange={handleChange2}
+                                            />
+                                        ) : (
+                                            <p className="addnotetxt">{BoxVal.content2}</p>
+                                        )}
                                     </div>
                                 </div>
                             </div>
@@ -241,26 +175,6 @@ function NotesPage(props) {
                     <div>No notes added</div>
                 )}
             </div>
-
-            {editmode ? (
-
-                <div className="editBox">
-
-
-                    <input className='form-control form-control-sm' type="text" value={editedTitle} onChange={(e) => { setEditedtitle(e.target.value) }} placeholder='Edit title' />
-
-                    <textarea rows={4} className='form-control form-control-sm mt-3 mb-3' type="text" value={editedContent} onChange={(e) => { setEditedcontent(e.target.value) }} placeholder='Edit content' />
-
-                    <button type='button' className='btn     btn-success btn-sm  mx-2' onClick={handleSave}>save</button>
-                    <button className='btn btn-sm btn-warning mx-2' onClick={handleCancel}>cancel</button>
-
-
-
-                </div>
-
-
-            ) : ""}
-
         </div>
     );
 }
